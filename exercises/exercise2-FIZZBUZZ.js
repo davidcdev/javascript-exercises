4a814805e855f4a0a51deaf219fb6a457ca0b9f3
DavidCDev <davidcastellanos.dev@gmail.com>
2023-05-16 11:56:57 -0500

    Corrected

--- exercises/exercise2-FIZZBUZZ.js
+++ exercises/exercise2-FIZZBUZZ.js
@@ -3,8 +3,7 @@ For numbers divisible by 3, print "Fizz" instead of the number, and for numbers 
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
 
-// SOLUTION WITH IFs & ELSE IFs
-for (let i = 1; i <= 100; i++) {
+ for (let i = 1; i <= 100; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
         console.log("FizzBuzz");
     } else if (i % 3 == 0) {
@@ -15,21 +14,3 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
-
-// SOLUTION WITH SWITCH
-for (let i = 1; i <= 100; i++) {
-    switch (i) {
-        case i % 5 == 0 && i % 3 == 0:
-            console.log("FizzBuzz");
-            break;
-        case i % 3 == 0:
-            console.log("Fizz");
-            break;
-        case i % 5 == 0 && i % 3 != 0:
-            console.log("Buzz");
-            break;
-        default: 
-        console.log(i);
-    }
-}
-// SWITCH SEEMS EASIER TO READ
